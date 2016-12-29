3e1431f9ff97b2e828c00b7bbab2499df0465a21
HuXue <you@example.com>
2016-12-29 13:09:38 +0800

    changed teamwork!

--- teamwork/teamwork.pl
+++ teamwork/teamwork.pl
@@ -1,13 +1,13 @@
 #!/usr/bin/perl -w
 use strict;
 my @bioth=('2016-12-16&2016-12-23','2016-12-30','2017-1-12');
-my @bioex=('No classroom discussion!','2016-12-12','2016-12-19');
+my @bioex=('No discussion!','2016-12-12','2016-12-19');
 my @perlth=('2017-1-4','2017-1-4','2017-1-9');
-my @perlex=('No classroom discussion!','2017-1-5','No exam!');
+my @perlex=('No discussion!','2017-1-5','No exam!');
 my @cbth=('2017-1-3','2017-1-3','2017-1-11');
-my @cbex=('No classroom discussion!','2016-12-9','2016-12-16','2016-12-23');
-my @pha=('No classroom discussion!','2017-1-4(Need to hand in your summary!)','2017-1-10');
-my @sp=('No classroom discussion!','2016-12-27','2017-1-3');
+my @cbex=('No discussion!','2016-12-9','2016-12-16','2016-12-23');
+my @pha=('No discussion!','2017-1-4(Need to hand in your summary!)','2017-1-10');
+my @sp=('No discussion!','2016-12-27','2017-1-3');
 my $num = '';
 do {
 print "\n################MENU################\n";
